9fb730f81543fd1bb6df074c750098f913799096
MacKenzie779 <MacKenzie779@proton.me>
2024-09-10 17:47:19 +0200

    rewrite history

--- rewriteGit.sh
+++ rewriteGit.sh
@@ -8,7 +8,7 @@ then
     export GIT_COMMITTER_NAME="$NEW_NAME"
     export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_NAME" ]
+if [ "$GIT_AUTHOR_NAME" = "$WRONG_NAME" ]
 then
     export GIT_AUTHOR_NAME="$NEW_NAME"
     export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
