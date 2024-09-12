b22a9f9f5dfc6d32f4f4b8ef6db0816af5c007ae
MacKenzie779 <MacKenzie779@proton.me>
2024-09-12 18:47:59 +0200

    adding hyprland conf

---
+++ postinstall/hyprland.sh
@@ -0,0 +1,23 @@
+paru -S hyprland hyprpaper hyprlock hyprshot --noconfirm --needed
+# rofi for wayland
+paru -S fuzzel --noconfirm --needed
+# status bar
+paru -S waybar --noconfirm --needed
+# terminal
+paru -S kitty --noconfirm --needed
+# icons
+paru -S kora-icon-theme phinger-cursors --noconfirm --needed
+# polkit
+paru -S lxpolkit --noconfirm --needed
+# notification deamon
+paru -S dunst --noconfirm --needed
+# cmd line utility for controlling media players
+paru -S playerctl --noconfirm --needed
+# file explorer
+paru -S nemo --noconfirm --needed
+
+echo "Deploying config by GamingGuy003"
+git clone https://github.com/GamingGuy003/configs-dotfiles
+cd fedora
+cd hyprland
+mv dotfiles ~/.config
